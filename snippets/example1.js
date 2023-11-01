2f7bae7d72d8d9442f458dc539ee05239adb600a
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-01 16:10:43 -0600

    js

--- snippets/example1.js
+++ snippets/example1.js
@@ -1,5 +1,10 @@
 let animals = ["kangaroo", "bear", "coyote"];
 
+// let key = element, index => element[index];
+// let key = element => element[0];
+// let key = (element, index) => element[--index];
+let key = (element, index) => element[index];
+
 if (animals.map(key).join("") === "key") {
     console.log("wild animals");
 }
