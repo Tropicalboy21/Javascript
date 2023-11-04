637788fa90111979e06c93f81f851ae075a7f0c3
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-04 15:49:09 -0600

    refresh

--- snippets/example1.js
+++ snippets/example1.js
@@ -3,6 +3,7 @@ let animals = ["kangaroo", "bear", "coyote"];
 // let key = element, index => element[index];
 // let key = element => element[0];
 // let key = (element, index) => element[--index];
+
 let key = (element, index) => element[index];
 
 if (animals.map(key).join("") === "key") {
