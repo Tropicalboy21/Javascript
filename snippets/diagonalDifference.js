de3e3ac14c3e48b7830f6786b9a8db50e6429989
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-04 08:29:58 -0600

    diagonal difference

--- snippets/diagonalDifference.js
+++ snippets/diagonalDifference.js
@@ -1,3 +1,19 @@
-function diagonalDifference() {
+function diagonalDifference(arr) {
+    let n = arr.length;
+    let diag1 = 0;
+    let diag2 = 0;
 
+    for (let i = 0; i < n.length; i++) {
+        for (let j = 0; j < n.length; j++) {
+            if (i === j) {
+                diag1 += arr[i][j];
+            }
+            if (i + j === n - 1) {
+                diag2 += arr[i][j];
+            }
+        }
+    }
+    return Math.abs(diag1 - diag2);
 }
+
+const a = [1, 2, 3, 4, 5, 6, 9, 8, 9];
