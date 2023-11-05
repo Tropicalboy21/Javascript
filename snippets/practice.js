43c73c1fff0acc1c613b9dd9e51ef91f487b4622
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-05 08:26:14 -0600

    practice js

--- snippets/practice.js
+++ snippets/practice.js
@@ -1,3 +1,35 @@
-let array = ["a", "b", "c"];
+// Arrays;
 
-document.getElementById("array").innerHTML = array;
+const array = ["apple", "bloomberg", "casio"];
+
+// Sets
+
+const brands = new Set(["Humblot", "Mercedes-Benz", "Samsung"]);
+
+
+let al = array.length;
+let bl = brands.size;
+
+
+if (al === 3 && bl === 3) {
+    array.push("apple");
+    brands.add("Humblot");
+}
+
+
+let element = "";
+
+array.forEach(function (value) {
+    element += value + " ✿ ";
+})
+
+let brand = "";
+
+brands.forEach(function (value) {
+    brand += value + " ✿ ";
+})
+
+
+
+document.getElementById("array").innerHTML = element;
+document.getElementById("sets").innerHTML = brand;
