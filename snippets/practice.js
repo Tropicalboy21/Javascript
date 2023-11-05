7e1bc237def452b2bb75a2d0441110468bc2ae17
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-05 17:46:38 -0600

    practice

--- snippets/practice.js
+++ snippets/practice.js
@@ -7,6 +7,10 @@ const array = ["apple", "bloomberg", "casio"];
 
 const brands = new Set(["Humblot", "Mercedes-Benz", "Samsung"]);
 
+// Array.map
+const symbols = ["hello", "nepal", "cumbia"];
+const numbers1 = [45, 4, 9, 16, 25];
+
 
 let al = array.length;
 let bl = brands.size;
@@ -29,7 +33,21 @@ brands.forEach(function (value) {
     brand += value + " ✿ ";
 })
 
-
-
 document.getElementById("array").innerHTML = element;
 document.getElementById("sets").innerHTML = brand;
+
+let combination = (element, index) => element[index];
+
+if (symbols.map(combination).join("") === "hem") {
+    console.log("Happy world");
+}
+const numbers2 = myFunction(numbers1);
+
+document.getElementById('map').innerHTML = numbers2;
+
+function myFunction(value) {
+    return value;
+}
+
+
+
