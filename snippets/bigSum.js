b01c20fbeddbe76d49ae0a3f99e40a56e46c5592
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-11 17:37:57 -0600

    js

--- snippets/bigSum.js
+++ snippets/bigSum.js
@@ -1,7 +1,7 @@
 let arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
 
 let bigSum = (x) => {
-    totalSum = 0;
+    let totalSum = 0;
 
     x.forEach(number => {
         totalSum += number
