846548ebe52504a7e9876f19116953baa3744fa7
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-11 17:37:41 -0600

    re factor

--- snippets/bigSum.js
+++ snippets/bigSum.js
@@ -1,8 +1,13 @@
 let arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
-totalSum = 0;
 
-arr.forEach(number => {
-    totalSum += number
-});
+let bigSum = (x) => {
+    totalSum = 0;
 
-console.log(totalSum);
+    x.forEach(number => {
+        totalSum += number
+    });
+
+    return totalSum;
+}
+
+bigSum(arr)
