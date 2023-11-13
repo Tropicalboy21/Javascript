88bc1055838ad4cf1499062d75afb693ca5c1738
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-13 16:21:06 -0600

    practice

--- snippets/basics.js
+++ snippets/basics.js
@@ -8,3 +8,26 @@ console.log(name, lastname, etnicity);
 
 // Data types
 
+// Numbers
+let length = 7;
+let width = 4.5;
+
+// String
+let string = "green"
+
+// Boolean
+let x = false;
+let y = true;
+
+// Object
+let car = {
+    brand: "Ferrari",
+    color: "red"
+};
+
+// Object Array
+let years = [2000, 2001, 2002, 2003, 2004, 2005];
+
+// Object Date
+
+let date = new Date("mm/dd/yyyy");
