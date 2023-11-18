b108a4c56b225a0f122ffdf918203fbdf852b3b0
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-18 16:01:10 -0600

    practice

--- snippets/miniMax.js
+++ snippets/miniMax.js
@@ -1,22 +1,21 @@
-let minimax = (arr) => {
-
+let miniMax = (arr) => {
     let min = Infinity;
     let max = -Infinity;
-
     let sum = 0;
 
     for (let i = 0; i < arr.length; i++) {
         const curr = arr[i];
+        sum += curr
         if (curr > max) {
             max = curr;
         }
         if (curr < min) {
             min = curr;
         }
-
-        console.log(sum - min, sum - max);
     }
+
+    console.log(sum - max, sum - min);
 };
 
 const a = [7, 69, 2, 221, 8974];
-miniMaxSum(a);
+miniMax(a);
