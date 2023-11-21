0323fa3df1e538f1a983174ed71fdfe43580a134
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-11-20 18:42:28 -0600

    practice js

---
+++ snippets/birthdayCakeCandles.js
@@ -0,0 +1,14 @@
+const arr = [1, 4, 2, 4, 5]
+
+let birthdayCakeCandles = (n) => {
+    let numCandles = 0;
+    let max = Math.max(...n)
+
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] === max) {
+            numCandles += 1;
+        }
+    }
+
+    return numCandles;
+}
