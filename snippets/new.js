c3ee954042607b9ca825a76731f1eb37ed496b7f
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-12-16 07:09:54 -0600

    practice

--- snippets/new.js
+++ snippets/new.js
@@ -12,7 +12,7 @@ function calculateAlternativeSyntax(input) {
 
     let result = 0;
 
-    if (stackOperands.length === 1) {
+    if (stackOperators.length === 1) {
 
         let operator = stackOperators[0];
         let operand1 = stackOperands[0];
@@ -32,30 +32,67 @@ function calculateAlternativeSyntax(input) {
                 result = operand1 / operand2
                 break;
         }
-    } else if (stackOperands.length >= 2) {
+    } else if (stackOperators.length >= 2) {
 
         let operator1 = stackOperators[0];
         let operator2 = stackOperators[stackOperators.length - 1];
         let operand1 = stackOperands[0]
-        let operand2 = stackOperands[0]
+        let operand2 = stackOperands[1]
+        let operand3 = stackOperands[2]
+        let temp = 0
 
+        switch (operator2) {
+            case '*':
+                temp = operand2 * operand3
+                break;
+            case '/':
+                temp = operand2 / operand3
+                break;
+            case '-':
+                temp = operand2 - operand3
+                break;
+            case '+':
+                temp = operand2 + operand3
+                break;
+            default:
+                break;
+        }
 
-        console.log(operator1, operator2);
+        switch (operator1) {
+            case '*':
+                result = operand1 * temp
+                break;
+            case '/':
+                result = operand1 / temp
+                break;
+            case '-':
+                result = operand1 - temp
+                break;
+            case '+':
+                result = operand1 + temp
+                break;
+            default:
+                break;
+        }
+
     }
 
-
-
-
-
-
-    console.log(stackOperands);
-    console.log(stackOperators);
     console.log(result);
-    console.log(operator);
+
 }
 
 
 // Example usage:
-const expression = '- 3 * 4 5';
-const expression2 = '- 3 4';
-calculateAlternativeSyntax(expression);
+const expression1 = '- 3 4';
+const expression2 = '- 3 * 4 5';
+const expression3 = '* + 3 4 5';
+calculateAlternativeSyntax(expression1);
+calculateAlternativeSyntax(expression2);
+calculateAlternativeSyntax(expression3);
+
+
+
+// console.log(stackOperands);
+// console.log(stackOperators);
+// console.log(result);
+// console.log(operator);
