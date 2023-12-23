4809be3d7c2f8a2ad5e7f67d2b915d0e2ff122fd
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-12-22 20:23:54 -0600

    practice

--- snippets/timeInWords.js
+++ snippets/timeInWords.js
@@ -3,44 +3,124 @@ function timeInWords(h, m) {
     let hour = h;
     let minutes = m;
 
-    let hours = ["one", "two", "three", "four", "five", "six"];
+    const units = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let decimals = ["ten", "eleven", "twelve", "thirteen", "fourteen", "sixteen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine"];
 
-    let time = 'time in words';
+    const parts = [" half ", " quater"];
+
+    const parts2 = [" o'clock", " to", " past "]
+
+    let timePt1 = 'hours';
+
+    let timePt2 = "minutes";
+
+    let finalTime = '';
 
     switch (h) {
         case 1:
-            time = "one";
+            timePt1 = units[0];
             break;
 
         case 2:
-            time = "two";
+            timePt1 = units[1];
             break;
 
         case 3:
-            time = "three";
+            timePt1 = units[2];
             break;
 
         case 4:
-            time = "four";
+            timePt1 = units[3];
             break;
 
         case 5:
-            time = "five";
+            timePt1 = units[4];
             break;
 
         case 6:
-            time = "six";
+            timePt1 = units[5];
+            break;
+
+        case 7:
+            timePt1 = units[6];
+            break;
+        case 8:
+            timePt1 = units[7];
+            break;
+        case 9:
+            timePt1 = units[8];
             break;
 
         default:
             break;
     }
 
+    switch (m) {
+        case 1:
+            timePt2 = units[0];
+            break;
+        case 2:
+            timePt2 = units[1];
+            break;
+        case 3:
+            timePt2 = units[2];
+            break;
+        case 4:
+            timePt2 = units[3];
+            break;
+        case 5:
+            timePt2 = units[4];
+            break;
+        case 6:
+            timePt2 = units[5];
+            break;
+        case 7:
+            timePt2 = units[6];
+            break;
+        case 8:
+            timePt2 = units[7];
+            break;
+        case 9:
+            timePt2 = units[8];
+            break;
+        case 10:
+            timePt2 = decimals[0];
+            break;
+        case 11:
+            timePt2 = decimals[1];
+            break;
+        case 12:
+            timePt2 = decimals[2];
+            break;
+        case 13:
+            timePt2 = decimals[13];
+            break;
+        case 14:
+            timePt2 = decimals[14];
+            break;
 
-    return time;
+        case 15:
+            timePt2 = parts[1];
+            break;
+        default:
+            break;
+    }
 
+    if (minutes === 0) {
+        timePt2 = parts2[0]
+        finalTime = timePt1.concat(timePt2);
+    } else if (minutes === 1) {
+        finalTime = timePt2 + " minute" + parts2[2] + timePt1;
+    } else if (minutes >= 2 && minutes <= 14) {
+        finalTime = timePt2 + " minutes" + parts2[2] + timePt1;
+    } else if (minutes === 15) {
+        finalTime = timePt2 + parts2[2] + timePt1;
+    }
+
+
+    // return time;
+    console.log(finalTime);
 
 }
 
-
-timeInWords(1, 4)
+timeInWords(6, 15)
