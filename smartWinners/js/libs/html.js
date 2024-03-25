493c9211034c1b83f7c067ab72e7f999baeb19a9
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-03-24 19:02:31 -0600

    test

--- smartWinners/js/libs/html.js
+++ smartWinners/js/libs/html.js
@@ -1,3 +1,25 @@
-const createElement = function (type, attributes, parent) {
+export const div = function (attribute, parent) {
+    return createElement('div', attribute, parent);
+}
 
+export const p = function (attribute, parent) {
+    return createElement('p', attribute, parent);
+}
+
+export const hr = function (attribute, parent) {
+    return createElement('hr', attribute, parent);
+}
+
+export const img = function (attribute, parent) {
+    return createElement('img', attribute, parent);
+}
+
+const createElement = function (type, attributes, parent) {
+    let element = document.createElement(type);
+
+    for (const attribute in attributes) {
+        element[attribute] = attributes[attribute];
+
+        return element;
+    }
 }
