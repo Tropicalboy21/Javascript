88ddf1a86e7ff9ebc0aca4680861d8826e69fb98
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-04-04 10:02:13 -0600

    idea

--- soundPlayer/js/index.js
+++ soundPlayer/js/index.js
@@ -1,34 +1,45 @@
-
 var intervalId;
+var countdownIntervalId;
 
 document.querySelector("#startButton").addEventListener("click", function () {
-
-  var start = new Audio("sounds/snare.mp3");
+  var start = new Audio("sounds/start.mp4");
   start.play();
 
-  intervalId = setInterval(function () {
-    var dilatation = new Audio("sounds/two-shot.mp4");
+  var remainingTime = 150; // Initial time in seconds (2.5 minutes)
+  updateTimerDisplay(remainingTime);
 
-    dilatation.play();
-  }, 150000);
+  countdownIntervalId = setInterval(function () {
+    remainingTime -= 1;
+    updateTimerDisplay(remainingTime);
+    if (remainingTime <= 0) {
+      clearInterval(countdownIntervalId);
+      var delatation = new Audio("sounds/delatation.mp4");
+      delatation.play();
+      countdownIntervalId = setInterval(function () {
+        remainingTime = 150;
+        updateTimerDisplay(remainingTime);
+      }, 150000);
+    }
+  }, 1000);
 
-
-  // document.querySelector("#startButoon").disabled = true;
-
-  // document.querySelector("#stopButton").disabled = false;
-
+  document.querySelector("#startButton").disabled = true;
+  document.querySelector("#stopButton").disabled = false;
 });
 
-
 document.querySelector("#stopButton").addEventListener("click", function () {
-
-  var end = new Audio("sounds/snare.mp3");
+  var end = new Audio("sounds/end.mp4");
   end.play();
 
   clearInterval(intervalId);
+  clearInterval(countdownIntervalId);
 
-  // document.querySelector("#startButoon").disabled = false;
+  document.querySelector("#startButton").disabled = false;
+  document.querySelector("#stopButton").disabled = true;
+});
 
-  // document.querySelector("#stopButton").disabled = true;
-
-})
+function updateTimerDisplay(seconds) {
+  var minutes = Math.floor(seconds / 60);
+  var remainingSeconds = seconds % 60;
+  var timerDisplay = document.querySelector("#timerDisplay");
+  timerDisplay.textContent = minutes + ":" + (remainingSeconds < 10 ? "0" : "") + remainingSeconds;
+}
