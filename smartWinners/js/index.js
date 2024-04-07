3abf2fd62226298a8e9b32a0727ab1addb1df9e7
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-04-07 10:43:21 -0600

    practice

--- smartWinners/js/index.js
+++ smartWinners/js/index.js
@@ -12,3 +12,16 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
--- smartWinners/js/view/cardView.js
+++ smartWinners/js/view/cardView.js
@@ -7,7 +7,7 @@ export class CardView {
 
         this.container = div({ id: 'cardContainer', className: 'cardContainer' }, this.parent);
         this.imgContainer = div({ id: 'imgContainer', className: 'img-container' }, this.container);
-        this.iconContainer = img({ id: 'iconContainer', className: 'icon_container', src: this.card.icon }, this.imgContainer);
+        this.iconContainer = img({ id: 'iconContainer', className: 'icon_container', src: this.card.icon, alt: 'card' }, this.imgContainer);
         this.infoContainer = div({ id: 'infoContainer', className: 'info_container' }, this.container);
         this.titleWrapper = div({ id: 'titleWrapper', className: 'title_wrapper' }, this.infoContainer)
         this.titleContainer = div({ id: 'titleContainer', className: 'title_container' }, this.titleWrapper);
