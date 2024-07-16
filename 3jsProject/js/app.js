5bd091b19197a6fff6b3cd20d840a37ef5b1e03e
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-07-15 18:32:33 -0600

    practice 3js

--- 3jsProject/js/app.js
+++ 3jsProject/js/app.js
@@ -1,185 +1,85 @@
 import * as THREE from 'three';
 
-import Stats from 'three/addons/libs/stats.module.js';
+			import Stats from 'three/addons/libs/stats.module.js';
 
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { Water } from 'three/addons/objects/Water.js';
-import { Sky } from 'three/addons/objects/Sky.js';
+			import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+			import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 
-let container, stats;
-let camera, scene, renderer;
-let controls, water, sun, mesh;
+			import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+			import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
-init();
-animate();
+			let mixer;
 
-function init() {
+			const clock = new THREE.Clock();
+			const container = document.getElementById( 'container' );
 
-    container = document.getElementById('container');
+			const stats = new Stats();
+			container.appendChild( stats.dom );
 
-    //
+			const renderer = new THREE.WebGLRenderer( { antialias: true } );
+			renderer.setPixelRatio( window.devicePixelRatio );
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			container.appendChild( renderer.domElement );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 0.5;
-    container.appendChild(renderer.domElement);
+			const pmremGenerator = new THREE.PMREMGenerator( renderer );
 
-    //
+			const scene = new THREE.Scene();
+			scene.background = new THREE.Color( 0xbfe3dd );
+			scene.environment = pmremGenerator.fromScene( new RoomEnvironment( renderer ), 0.04 ).texture;
 
-    scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
+			camera.position.set( 5, 2, 8 );
 
-    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
-    camera.position.set(30, 30, 100);
+			const controls = new OrbitControls( camera, renderer.domElement );
+			controls.target.set( 0, 0.5, 0 );
+			controls.update();
+			controls.enablePan = false;
+			controls.enableDamping = true;
 
-    //
+			const dracoLoader = new DRACOLoader();
+			dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
 
-    sun = new THREE.Vector3();
+			const loader = new GLTFLoader();
+			loader.setDRACOLoader( dracoLoader );
+			loader.load( 'models/gltf/LittlestTokyo.glb', function ( gltf ) {
 
-    // Water
+				const model = gltf.scene;
+				model.position.set( 1, 1, 0 );
+				model.scale.set( 0.01, 0.01, 0.01 );
+				scene.add( model );
 
-    const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+				mixer = new THREE.AnimationMixer( model );
+				mixer.clipAction( gltf.animations[ 0 ] ).play();
 
-    water = new Water(
-        waterGeometry,
-        {
-            textureWidth: 512,
-            textureHeight: 512,
-            waterNormals: new THREE.TextureLoader().load('textures/waternormals.jpg', function (texture) {
+				renderer.setAnimationLoop( animate );
 
-                texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+			}, undefined, function ( e ) {
 
-            }),
-            sunDirection: new THREE.Vector3(),
-            sunColor: 0xffffff,
-            waterColor: 0x001e0f,
-            distortionScale: 3.7,
-            fog: scene.fog !== undefined
-        }
-    );
+				console.error( e );
 
-    water.rotation.x = - Math.PI / 2;
+			} );
 
-    scene.add(water);
 
-    // Skybox
+			window.onresize = function () {
 
-    const sky = new Sky();
-    sky.scale.setScalar(10000);
-    scene.add(sky);
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
 
-    const skyUniforms = sky.material.uniforms;
+				renderer.setSize( window.innerWidth, window.innerHeight );
 
-    skyUniforms['turbidity'].value = 10;
-    skyUniforms['rayleigh'].value = 2;
-    skyUniforms['mieCoefficient'].value = 0.005;
-    skyUniforms['mieDirectionalG'].value = 0.8;
+			};
 
-    const parameters = {
-        elevation: 2,
-        azimuth: 180
-    };
 
-    const pmremGenerator = new THREE.PMREMGenerator(renderer);
-    const sceneEnv = new THREE.Scene();
+			function animate() {
 
-    let renderTarget;
+				const delta = clock.getDelta();
 
-    function updateSun() {
+				mixer.update( delta );
 
-        const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
-        const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+				controls.update();
 
-        sun.setFromSphericalCoords(1, phi, theta);
+				stats.update();
 
-        sky.material.uniforms['sunPosition'].value.copy(sun);
-        water.material.uniforms['sunDirection'].value.copy(sun).normalize();
+				renderer.render( scene, camera );
 
-        if (renderTarget !== undefined) renderTarget.dispose();
-
-        sceneEnv.add(sky);
-        renderTarget = pmremGenerator.fromScene(sceneEnv);
-        scene.add(sky);
-
-        scene.environment = renderTarget.texture;
-
-    }
-
-    updateSun();
-
-    //
-
-    const geometry = new THREE.BoxGeometry(30, 30, 30);
-    const material = new THREE.MeshStandardMaterial({ roughness: 0 });
-
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    //
-
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.maxPolarAngle = Math.PI * 0.495;
-    controls.target.set(0, 10, 0);
-    controls.minDistance = 40.0;
-    controls.maxDistance = 200.0;
-    controls.update();
-
-    //
-
-    stats = new Stats();
-    container.appendChild(stats.dom);
-
-    // GUI
-
-    const gui = new GUI();
-
-    const folderSky = gui.addFolder('Sky');
-    folderSky.add(parameters, 'elevation', 0, 90, 0.1).onChange(updateSun);
-    folderSky.add(parameters, 'azimuth', - 180, 180, 0.1).onChange(updateSun);
-    folderSky.open();
-
-    const waterUniforms = water.material.uniforms;
-
-    const folderWater = gui.addFolder('Water');
-    folderWater.add(waterUniforms.distortionScale, 'value', 0, 8, 0.1).name('distortionScale');
-    folderWater.add(waterUniforms.size, 'value', 0.1, 10, 0.1).name('size');
-    folderWater.open();
-
-    //
-
-    window.addEventListener('resize', onWindowResize);
-
-}
-
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-}
-
-function animate() {
-
-    requestAnimationFrame(animate);
-    render();
-    stats.update();
-
-}
-
-function render() {
-
-    const time = performance.now() * 0.001;
-
-    mesh.position.y = Math.sin(time) * 20 + 5;
-    mesh.rotation.x = time * 0.5;
-    mesh.rotation.z = time * 0.51;
-
-    water.material.uniforms['time'].value += 1.0 / 60.0;
-
-    renderer.render(scene, camera);
-
-}
+			}
