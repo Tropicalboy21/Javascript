12877ae90cb9094b14e3cf5d2fe5acf87307e7dc
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-08-27 22:11:59 -0600

    New portafolio version

--- 3js/js/script.js
+++ 3js/js/script.js
@@ -1,9 +1,54 @@
-function sendEmail() {
-    let params = {
-        name : document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
+
+function hideErrorOnInput(inputId, errorId) {
+    document.getElementById(inputId).addEventListener("input", function() {
+        document.getElementById(errorId).style.display = "none";
+    });
+}
+
+hideErrorOnInput("name", "req");
+hideErrorOnInput("email", "req2");
+hideErrorOnInput("message", "req3");
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let isValid = true;
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (!name) {
+        document.getElementById("req").style.display = "block";
+        isValid = false;
     }
 
-    emailjs.send("service_pcakkqr","template_kclosz7", params).then(alert("Email sent successfully"));
-}
+    if (!email) {
+        document.getElementById("req2").style.display = "block";
+        isValid = false;
+    }
+
+    if (!message) {
+        document.getElementById("req3").style.display = "block";
+        isValid = false;
+    }
+
+    if (isValid) {
+        let params = {
+            name: name,
+            email: email,
+            message: message
+        };
+
+        const serviceID = 'service_pcakkqr';
+        const templateID = 'template_kclosz7';
+
+        emailjs.send(serviceID, templateID, params)
+        .then(() => {
+            alert('Sent!');
+            document.getElementById("contact-form").reset();
+        }, (err) => {
+            alert(JSON.stringify(err));
+        });
+    }
+});
