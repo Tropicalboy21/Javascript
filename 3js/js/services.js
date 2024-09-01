b6bf22af82065193022b8f7b00b09f53720022f4
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-09-01 15:41:36 -0600

    New portafolio site

--- 3js/js/services.js
+++ 3js/js/services.js
@@ -1,14 +1,36 @@
 // const { title } = require("process");
 
-document.querySelectorAll('.service').forEach(service => {
-    service.addEventListener('click', function() {
-        document.querySelectorAll('.service').forEach(s => s.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
+// document.querySelectorAll('.service').forEach(service => {
+//     service.addEventListener('click', function() {
+//         document.querySelectorAll('.service').forEach(s => s.classList.remove('active'));
+//         this.classList.add('active');
+//     });
+// });
 
-document.addEventListener('click', function(event) {
-    if (!event.target.closest('.service.active')) {
-        document.querySelectorAll('.service').forEach(s => s.classList.remove('active'));
+// document.addEventListener('click', function(event) {
+//     if (!event.target.closest('.service.active')) {
+//         document.querySelectorAll('.service').forEach(s => s.classList.remove('active'));
+//     }
+// });
+
+const cards = document.querySelectorAll('.service');
+
+function showCard() {
+
+    const cardContainer = document.getElementById('card-container');
+    const closeButton = document.getElementById('close-icon');
+
+    function hideCard() {
+        cardContainer.classList.add("hide");
+        setTimeout(function() {
+            cardContainer.style.display = "none";
+            cardContainer.classList.remove("hide");
+        }, 5000);
     }
-});
+
+    closeButton.addEventListener("click", hideAlert);
+
+}
+
+cards.forEach(card => card.addEventListener('click', showCard));
+
